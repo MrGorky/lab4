fe36fc063c61377c69e6fa1fb7b6578f84c3cb3e
Драганчук Алена Сергеевна <ri57s05@awp10.csc.local>
2016-05-31 10:49:15 +0600

    Add func

---
+++ src/func.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int sum(int a, int b){
+	int rez;
+	rez = a + b;
+	return rez;
+}
+
+int raz(int a, int b){
+	int rez;
+	rez = a - b;
+	return rez;
+}
+
+int del(int a, int b){
+	int rez;
+	rez = a / b;
+	return rez;
+}
+
+int proz(int a, int b){
+	int rez;
+	rez = arg1 * arg2;
+	return rez;
+}
